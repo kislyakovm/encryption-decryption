113c10380479a48d7591a44391ebf869f984b6d9
kislyakovm <maksim@druble.ru>
2023-02-16 02:17:34 +0300

    - add decryption

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -7,25 +7,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String operation = scanner.nextLine();
         String str = scanner.nextLine();
         int key = scanner.nextInt();
 
-        int c = 0;
-        int y = 0;
-
-        str = str.toLowerCase();
-        StringBuilder decryptedStr = new StringBuilder();
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ' ') {
-                decryptedStr.append(' ');
-                continue;
-            }
-            c = (str.charAt(i) + key);
-            y = (c - 97) %26 + 97;
-            decryptedStr.append((char) y);
+        if ("enc".equals(operation)) {
+            encrypting(str, key);
+        } else if ("dec".equals(operation)) {
+            decrypting(str, key);
         }
+    }
 
-        System.out.println(decryptedStr);
+    public static void encrypting(String str, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char character : str.toCharArray()) {
+            char newCharacter = (char) (character + key);
+            result.append(newCharacter);
+        }
+        System.out.println(result);
+    }
+
+
+    public static void decrypting(String str, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char character : str.toCharArray()) {
+            char newCharacter = (char) (character - key);
+            result.append(newCharacter);
+        }
+        System.out.println(result);
     }
 }
