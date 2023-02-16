d5bef1f76464680859c7e3688f05805593dfe7bd
kislyakovm <maksim@druble.ru>
2023-02-17 01:58:15 +0300

    - add command-line arguments

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,15 +1,35 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int key;
+        String operation;
+        String str;
 
-        String operation = scanner.nextLine();
-        String str = scanner.nextLine();
-        int key = scanner.nextInt();
+        if ("-key".equals(args[0])) {
+            key = Integer.parseInt(args[1]);
+            if ("-mode".equals(args[2])) {
+                operation = args[3];
+                str = args[5];
+            } else {
+                operation = args[5];
+                str = args[3];
+            }
+        } else if ("-key".equals(args[2])) {
+            key = Integer.parseInt(args[3]);
+            if ("-mode".equals(args[0])) {
+                operation = args[1];
+                str = args[5];
+            } else {
+                operation = args[5];
+                str = args[1];
+            }
+        } else {
+            key = Integer.parseInt(args[5]);
+            operation = args[1];
+            str = args[3];
+        }
 
         if ("enc".equals(operation)) {
             encrypting(str, key);
