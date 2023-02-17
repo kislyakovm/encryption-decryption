db0505a487812b8a3418c3db699d0cea6e501186
kislyakovm <maksim@druble.ru>
2023-02-18 01:21:43 +0300

    - add work with files

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,59 +1,93 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int key;
-        String operation;
-        String str;
+    static String mode = "enc";
+    static int key = 0;
+    static String data = "";
+    static boolean isFile = false;
+    static String in = "";
+    static String out = "";
+    static String result = "";
 
-        if ("-key".equals(args[0])) {
-            key = Integer.parseInt(args[1]);
-            if ("-mode".equals(args[2])) {
-                operation = args[3];
-                str = args[5];
-            } else {
-                operation = args[5];
-                str = args[3];
-            }
-        } else if ("-key".equals(args[2])) {
-            key = Integer.parseInt(args[3]);
-            if ("-mode".equals(args[0])) {
-                operation = args[1];
-                str = args[5];
-            } else {
-                operation = args[5];
-                str = args[1];
-            }
+    public static void main(String[] args) throws FileNotFoundException {
+
+        defineAttributes(args);
+
+        if (isFile) {
+            readFromFile();
+            chooseEncOrDec();
+            writeToFile();
         } else {
-            key = Integer.parseInt(args[5]);
-            operation = args[1];
-            str = args[3];
-        }
-
-        if ("enc".equals(operation)) {
-            encrypting(str, key);
-        } else if ("dec".equals(operation)) {
-            decrypting(str, key);
+            chooseEncOrDec();
+            System.out.println(result);
         }
     }
 
-    public static void encrypting(String str, int key) {
+    public static void chooseEncOrDec() {
+        if ("enc".equals(mode)) {
+            result = encrypting(data, key);
+        } else if ("dec".equals(mode)) {
+            result = decrypting(data, key);
+        }
+    }
+
+    public static void readFromFile() throws FileNotFoundException {
+        File file = new File(in);
+        Scanner scanner = new Scanner(file);
+
+        data = scanner.nextLine();
+        scanner.close();
+    }
+
+    public static void writeToFile(){
+        File file = new File(out);
+
+        try (PrintWriter pw = new PrintWriter(file)){
+            pw.println(result);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void defineAttributes(String[] args) {
+        for (int i = 0; i < args.length; i += 2) {
+            if ("-mode".equals(args[i])) {
+                mode = args[i + 1];
+            } else if ("-key".equals(args[i])) {
+                key = Integer.parseInt(args[i + 1]);
+            } else if ("-data".equals(args[i])) {
+                data = args[i + 1];
+            } else if ("-in".equals(args[i])) {
+                in = args[i + 1];
+                isFile = true;
+            } else if ("-out".equals(args[i])) {
+                out = args[i + 1];
+                isFile = true;
+            }
+        }
+    }
+
+    public static String encrypting(String str, int key) {
         StringBuilder result = new StringBuilder();
         for (char character : str.toCharArray()) {
             char newCharacter = (char) (character + key);
             result.append(newCharacter);
         }
-        System.out.println(result);
+        return String.valueOf(result);
     }
 
-
-    public static void decrypting(String str, int key) {
+    public static String decrypting(String str, int key) {
         StringBuilder result = new StringBuilder();
         for (char character : str.toCharArray()) {
             char newCharacter = (char) (character - key);
             result.append(newCharacter);
         }
-        System.out.println(result);
+        return String.valueOf(result);
     }
 }
